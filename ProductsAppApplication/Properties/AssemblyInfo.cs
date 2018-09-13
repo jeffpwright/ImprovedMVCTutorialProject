a04642c472ee2f9415dadc1ad726085c5b27a694
Jeff Wright <jeffpwright@verizon.net>
2018-09-13 15:15:55 -0500

    renamed ProductsAppApplication

--- ProductsAppApplication/Properties/AssemblyInfo.cs
+++ ProductsAppApplication/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ProductsAppApplication2")]
+[assembly: AssemblyTitle("ProductsAppApplication")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ProductsAppApplication2")]
+[assembly: AssemblyProduct("ProductsAppApplication")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
